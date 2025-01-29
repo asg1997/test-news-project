00fce8483af3129bbb75de690ce3d2102afd6c58
asg1997 <34771066+asg1997@users.noreply.github.com>
2025-01-29 13:18:45 +0300

    начальный проект

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import { NewsPageController } from './controllers/news_page_controller.js';
-window.onload = function () {
-    const pageController = new NewsPageController(document);
-    pageController.initNewsPage();
-};
